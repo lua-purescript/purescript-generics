e4ab32a92e161321e8fb6302f8047240a13a107b
Martin Zeller <mz.bremerhaven@gmail.com>
2016-07-07 18:36:31 +0200

    Convert to JS

--- src/Data/Generic.lua
+++ src/Data/Generic.lua
@@ -1,41 +1,43 @@
-"use strict";
+-- module Data.Generic
 
-// module Data.Generic
+local exports = {}
 
-exports.zipAll = function (f) {
-  return function (xs) {
-    return function (ys) {
-      var l = xs.length < ys.length ? xs.length : ys.length;
-      for (var i = 0; i < l; i++) {
-        if (!f(xs[i])(ys[i])) {
-          return false;
-        }
-      }
-      return true;
-    };
-  };
-};
+exports.zipAll = function (f)
+  return function (xs)
+    return function (ys)
+	  local l = #xs < #ys and #xs or #ys
+	  for i=1, i<=l do
+		if not f(xs[i])(ys[i]) then
+          return false
+		  end
+	  end
+      return true
+    end
+  end
+end
 
-exports.zipCompare = function (f) {
-  return function (xs) {
-    return function (ys) {
-      var i = 0;
-      var xlen = xs.length;
-      var ylen = ys.length;
-      while (i < xlen && i < ylen) {
-        var o = f(xs[i])(ys[i]);
-        if (o !== 0) {
-          return o;
-        }
-        i++;
-      }
-      if (xlen === ylen) {
-        return 0;
-      } else if (xlen > ylen) {
-        return -1;
-      } else {
-        return 1;
-      }
-    };
-  };
-};
+exports.zipCompare = function (f)
+  return function (xs)
+    return function (ys)
+	  local i = 1
+	  local xlen = #xs
+	  local ylen = #ys
+      while (i <= xlen and i <= ylen) do
+        local o = f(xs[i])(ys[i])
+        if o ~= 0 then
+          return o
+		end
+		i = i + 1
+	  end
+      if (xlen === ylen) then
+        return 0
+      elseif (xlen > ylen) then
+        return -1
+      else
+        return 1
+	  end
+	end
+  end
+end
+
+return exports
